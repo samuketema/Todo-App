a174cf66e5576579f4e44a207ed9569596843ada
Samuel <samuelketema01@gmail.com>
2024-08-08 21:20:41 +0300

    This is the home page

---
+++ lib/screens/task.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import '../widgets/listview.dart';
+import '../widgets/tilelist.dart';
+
+class TaskList extends StatelessWidget {
+  const TaskList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.lightBlueAccent,
+        onPressed: () {},
+        child: Icon(
+          Icons.add,
+          color: Colors.white,
+        ),
+      ),
+      backgroundColor: Colors.lightBlueAccent,
+      body: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+        Container(
+          padding: EdgeInsets.only(top: 60, left: 40, bottom: 50, right: 20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              CircleAvatar(
+                  backgroundColor: Colors.white,
+                  radius: 30,
+                  child: Icon(
+                    Icons.list,
+                    size: 30,
+                    color: Colors.lightBlueAccent,
+                  )),
+              SizedBox(
+                height: 10,
+              ),
+              Text(
+                'Todoey',
+                style: TextStyle(
+                    fontSize: 40,
+                    color: Colors.white,
+                    fontWeight: FontWeight.w700),
+              ),
+              Text(
+                '12 Tasks',
+                style: TextStyle(color: Colors.white),
+              )
+            ],
+          ),
+        ),
+        Expanded(
+          child: Container(
+            height: 300,
+            decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(20),
+                    topRight: Radius.circular(20))),
+            child: TaskLIst(),
+          ),
+        )
+      ]),
+    );
+  }
+}
