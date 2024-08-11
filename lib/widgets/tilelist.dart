291de09537d0b9d0213005f53f38571d2f9700b3
Samuel <samuelketema01@gmail.com>
2024-08-11 17:46:22 +0300

    Statefull widget turned to stateless and refactored

--- lib/widgets/tilelist.dart
+++ lib/widgets/tilelist.dart
@@ -1,42 +1,25 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
-class TileList extends StatefulWidget {
-  @override
-  State<TileList> createState() => _TileListState();
-}
+class TileList extends StatelessWidget {
+  bool? isChecked;
+  String? title;
+  Function(bool?) onChanged;
 
-class _TileListState extends State<TileList> {
-  bool isChecked = false;
-  
-
+  TileList({this.isChecked, this.title, required this.onChanged});
   @override
   Widget build(BuildContext context) {
     return ListTile(
-        title: Text('Buy Milk' ,style: TextStyle(decoration: isChecked? TextDecoration.lineThrough:null),),
-        trailing: TaskCheckBox(
-          isChecked: isChecked,
-          toggleCheckBox:(bool? checkBoxState) {
-    setState(() {
-      isChecked = checkBoxState as bool;
-    });
-  }
-        ));
-  }
-}
-
-class TaskCheckBox extends StatelessWidget {
- void Function(bool?)? toggleCheckBox;
-  late bool isChecked;
-  TaskCheckBox({
-    
-    required this.toggleCheckBox,
-    required this.isChecked,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Checkbox(activeColor: Colors.lightBlueAccent,value: isChecked, onChanged: toggleCheckBox );
+        title: Text(
+          title as String,
+          style: TextStyle(
+              decoration: isChecked! ? TextDecoration.lineThrough : null),
+        ),
+        trailing: Checkbox(
+            activeColor: Colors.lightBlueAccent,
+            value: isChecked,
+            onChanged: onChanged));
   }
 }
