695ce526ffc78ba6921eb6ae48d9e253fd1839a9
Samuel <samuelketema01@gmail.com>
2024-08-11 17:45:25 +0300

    Stateless widget turned to stateful and refactored.

--- lib/widgets/listview.dart
+++ lib/widgets/listview.dart
@@ -1,19 +1,34 @@
 import 'package:flutter/material.dart';
 import 'tilelist.dart';
+import '../modal/task1.dart';
 
-class TaskLIst extends StatelessWidget {
-  const TaskLIst({
-    super.key,
-  });
+class TaskLists extends StatefulWidget {
+  const TaskLists({super.key});
 
   @override
+  State<TaskLists> createState() => _TaskListsState();
+}
+
+class _TaskListsState extends State<TaskLists> {
+  List<Task> tasks = [Task(name: 'First Task'),
+  Task(name: 'Second Task'),Task(name: "Third Task")];
+  @override
   Widget build(BuildContext context) {
-    return ListView(
-      padding: EdgeInsets.only(left: 30, top: 40, right: 15),
-      children: [
-        TileList(),
-        TileList(),
-      ],
+    return ListView.builder(
+      itemBuilder: (context, index) {
+        return TileList(
+          isChecked: tasks[index].isChecked,
+          title: tasks[index].name,
+          onChanged: (bool? checkBoxState) {
+            setState(
+              () {
+                tasks[index].toggleCheckBox();
+              },
+            );
+          },
+        );
+      },
+      itemCount: tasks.length,
     );
   }
 }
