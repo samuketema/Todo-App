9e3d5eae816744e8d3dc19c8bab812477dd0fd2c
Samuel <samuelketema01@gmail.com>
2024-08-13 23:22:03 +0300

    Code format customized.

--- lib/widgets/listview.dart
+++ lib/widgets/listview.dart
@@ -4,21 +4,21 @@ import '../modal/task1.dart';
 import '../screens/task.dart';
 
 class TaskLists extends StatefulWidget {
-   TaskLists({required this.tasks,});
-List<Task> tasks;
+  TaskLists({
+    required this.tasks,
+  });
+  List<Task> tasks;
   @override
   State<TaskLists> createState() => _TaskListsState();
 }
 
 class _TaskListsState extends State<TaskLists> {
-  
-  
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
       itemBuilder: (context, index) {
         return TileList(
-          isChecked: widget.tasks[index].isChecked, 
+          isChecked: widget.tasks[index].isChecked,
           title: widget.tasks[index].name,
           onChanged: (bool? checkBoxState) {
             setState(
