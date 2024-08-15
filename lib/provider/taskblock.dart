f20fa533eba77e91f11fbffcacc53805d9dc5e00
Samuel <samuelketema01@gmail.com>
2024-08-15 18:23:25 +0300

    Make the list private to increase security.

--- lib/provider/taskblock.dart
+++ lib/provider/taskblock.dart
@@ -1,19 +1,25 @@
+import 'dart:collection';
+
 import 'package:flutter/foundation.dart';
 import 'package:todo/modal/task1.dart';
 
 class TaskData extends ChangeNotifier {
-  List<Task> tasks = [
+  List<Task> _tasks = [
     Task(name: 'First Task'),
     Task(name: 'Second Task'),
     Task(name: "Third Task")
   ];
 
   int get numberOfTasks{
-    return tasks.length;
+    return _tasks.length;
+  }
+
+  UnmodifiableListView<Task> get tasks {
+    return UnmodifiableListView(_tasks);
   }
   
   void addNewTask(newTask){
-    tasks.add(Task(name: newTask));
+    _tasks.add(Task(name: newTask));
     notifyListeners();
   }
   void updateTask(Task task){
@@ -22,7 +28,7 @@ class TaskData extends ChangeNotifier {
   }
 
   void removeTask(Task task){
-    tasks.remove(task);
+    _tasks.remove(task);
     notifyListeners();
   }
 }
