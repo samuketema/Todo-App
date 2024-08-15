eb9900b8ebcb1b2987e20b231984a8e2bae76644
Samuel <samuelketema01@gmail.com>
2024-08-15 16:17:51 +0300

    Using consumer to get provided data by the provider.

--- lib/widgets/listview.dart
+++ lib/widgets/listview.dart
@@ -1,30 +1,33 @@
 import 'package:flutter/material.dart';
 import 'package:todo/provider/taskblock.dart';
 import 'tilelist.dart';
-import '../modal/task1.dart';
-import '../screens/task.dart';
 import 'package:provider/provider.dart';
 
 class TaskLists extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    List<Task> tasks =  Provider.of<TaskData>(context).tasks;
-    return ListView.builder(
-      itemBuilder: (context, index) {
-        return TileList(
-          isChecked: tasks[index].isChecked,
-          title: tasks[index].name,
-          onChanged: (bool? checkBoxState) {
-            // setState(
-            //   () {
-            //     tasks[index].toggleCheckBox();
-            //   },
-            // );
-          },
-        );
+
+   
+    return Consumer<TaskData>(
+      builder: (context, taskData, child) {
+        return ListView.builder(
+        itemBuilder: (context, index) {
+          return TileList(
+            isChecked: taskData.tasks[index].isChecked,
+            title: taskData.tasks[index].name,
+            onChanged: (bool? checkBoxState) {
+              // setState(
+              //   () {
+              //     tasks[index].toggleCheckBox();
+              //   },
+              // );
+            },
+          );
+        },
+        itemCount: taskData.tasks.length,
+      );
       },
-      itemCount: tasks.length,
     );
   }
 }
