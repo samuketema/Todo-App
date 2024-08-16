67217676e2e7fda5a776594baf020f218e5c7b0b
Samuel <samuelketema01@gmail.com>
2024-08-16 23:19:20 +0300

    Code format

--- lib/widgets/listview.dart
+++ lib/widgets/listview.dart
@@ -4,27 +4,26 @@ import 'tilelist.dart';
 import 'package:provider/provider.dart';
 
 class TaskLists extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
-
     return Consumer<TaskData>(
       builder: (context, taskData, child) {
-        
         return ListView.builder(
-        itemBuilder: (context, index) {
-          final currentTask = taskData.tasks[index];
-          return TileList(
-            onLongPress:(){taskData.removeTask(currentTask);} ,
-            isChecked: currentTask.isChecked,
-            title: currentTask.name,
-            onChanged: (bool? checkBoxState) {
-              taskData.updateTask(currentTask);
-            },
-          );
-        },
-        itemCount: taskData.numberOfTasks,
-      );
+          itemBuilder: (context, index) {
+            final currentTask = taskData.tasks[index];
+            return TileList(
+              onLongPress: () {
+                taskData.removeTask(currentTask);
+              },
+              isChecked: currentTask.isChecked,
+              title: currentTask.name,
+              onChanged: (bool? checkBoxState) {
+                taskData.updateTask(currentTask);
+              },
+            );
+          },
+          itemCount: taskData.numberOfTasks,
+        );
       },
     );
   }
